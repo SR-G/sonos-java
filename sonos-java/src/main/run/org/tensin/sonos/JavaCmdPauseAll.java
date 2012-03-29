4033583525f0176f290e1e59303fca5fd7d2029b
Sergio <serge.simon@gmail.com>
2012-03-29 23:01:37 +0200

    Run for java commands

---
+++ sonos-java/src/main/run/org/tensin/sonos/JavaCmdPauseAll.java
@@ -0,0 +1,22 @@
+package org.tensin.sonos;
+
+import org.tensin.sonos.commander.JavaCommander;
+import org.tensin.sonos.upnp.SonosException;
+
+/**
+ * The Class CmdPlaySalon.
+ */
+public class JavaCmdPauseAll {
+
+    /**
+     * The main method.
+     * 
+     * @param args
+     *            the arguments
+     * @throws SonosException
+     *             the sonos exception
+     */
+    public static void main(final String[] args) throws SonosException {
+        new JavaCommander().execute("pause");
+    }
+}
